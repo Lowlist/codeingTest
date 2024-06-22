85e5e348f2d1c8228373e4f5c45ccfdab1a0b3fb
Lowlist <152842060+Lowlist@users.noreply.github.com>
2024-06-22 21:50:45 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,17 @@
+function solution(angle) {
+    var answer = 0;
+    if(angle<90){
+        answer = 1;
+    }
+    if(angle == 90){
+        answer = 2;
+    }
+    if(angle>90 && angle<180){
+        answer = 3;
+    }
+    if(angle == 180){
+        answer = 4;
+    }
+    
+    return answer;
+}
