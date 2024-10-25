a3d2e23597a7d2c16bf8a86bde16713250c3caec
LowList <152842060+Lowlist@users.noreply.github.com>
2024-10-25 23:31:26 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.74 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    var answer = [];
+    for(let i=0;i<my_string.length;i++){
+        if(my_string[i] === my_string[i].toLowerCase()){
+            answer.push(my_string[i].toUpperCase());
+        }else{
+            answer.push(my_string[i].toLowerCase());
+        }
+    }
+    return answer.join("")
+}
